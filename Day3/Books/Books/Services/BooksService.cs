60e0e2abb3469993fb5cf2cc5d588ec5d1aa5b34
Neeraj Sharma <84141574+pillar008@users.noreply.github.com>
2024-05-24 13:10:48 +0530

    Added Day 3

---
+++ Day3/Books/Books/Services/BooksService.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Books.Models;
+
+namespace Books.Services
+{
+    public class BooksService
+    {
+        private readonly List<Book> _books;
+        private int _nextId = 2; // Start from 2 because we have one book with Id = 1
+
+        public BooksService()
+        {
+            _books = new List<Book>
+            {
+                new Book { Id = 1, Title = "1984", Author = "George Orwell", Genre = "Dystopian" },
+            };
+        }
+
+        public List<Book> GetAll()
+        {
+            return _books;
+        }
+
+        public Book GetById(int id) => _books.FirstOrDefault(b => b.Id == id);
+
+        public void Add(Book book)
+        {
+            book.Id = _nextId++;
+            _books.Add(book);
+        }
+
+        public void Update(Book book)
+        {
+            var index = _books.FindIndex(b => b.Id == book.Id);
+            if (index != -1)
+            {
+                _books[index] = book;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var book = GetById(id);
+            if (book != null)
+            {
+                _books.Remove(book);
+            }
+        }
+    }
+}
